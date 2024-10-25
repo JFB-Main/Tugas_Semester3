8b31e7d064f83404eac9dc0e09f16e6dc1600b02
Javier Farel Baroto <javierfarelb@gmail.com>
2024-10-25 13:46:27 +0700

    menambahkan tugas 4 for each

--- Tugas_For_and_ForEach/nomor4.php
+++ Tugas_For_and_ForEach/nomor4.php
@@ -9,13 +9,34 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <?php
     $array = [
-        ['produkA', 'iniprodukA', 'Rp.1000'],
-        [4, 5, 6],
-        [7, 8, 9]
+        ['produkA', 'iniprodukA', 'Rp.1000', 'https://i.ibb.co.com/yYSDsfk/Screenshot-2024-10-25-131055.png'],
+        ['produkB', 'iniprodukB', 'Rp.2000', 'https://i.ibb.co.com/yYSDsfk/Screenshot-2024-10-25-131055.png'],
+        ['produkC', 'iniprodukC', 'Rp.3000', 'https://i.ibb.co.com/yYSDsfk/Screenshot-2024-10-25-131055.png'],
+        ['produkD', 'iniprodukD', 'Rp.4000', 'https://i.ibb.co.com/yYSDsfk/Screenshot-2024-10-25-131055.png'],
+        ['produkE', 'iniprodukE', 'Rp.5000', 'https://i.ibb.co.com/yYSDsfk/Screenshot-2024-10-25-131055.png'],
+        ['produkF', 'iniprodukF', 'Rp.6000', 'https://i.ibb.co.com/yYSDsfk/Screenshot-2024-10-25-131055.png']
     ];
 
     // Access an element (e.g., 5)
-    echo $matrix[1][1]; // Output: 5
+    foreach ($array as $array) {
+        echo '<div class="card" style="width: 18rem;">
+        <img src="';
+        echo $array[3];
+        echo '" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">';
+        echo $array[0];
+        echo '</h5>
+            <p class="card-text">';
+        echo $array[1];
+        echo '</p>
+            <a href="#" class="btn btn-primary">';
+        echo $array[2];
+        echo '</a>
+        </div>
+        </div>';
+        }
+    
     ?>
 </body>
 </html>
